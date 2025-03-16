9b8097b6162ebfb3b374a9ce8b00a683f44756bc
alik <mamedovalik267@gmail.com>
2025-03-16 05:45:11 +0400

    aaaa

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func init() {
 	oauthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "http://localhost:3000/auth/google/callback",
+		RedirectURL:  "https://www.auth-user.ru/auth/google/callback",
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
